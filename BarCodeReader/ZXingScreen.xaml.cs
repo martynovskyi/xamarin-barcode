15370053c6198f829096dfceb17519468a6a4b87
Vitalii Martynovskyi <vitaliy.martynovskyy@ukr.net>
2019-09-16 12:18:13 +0300

    initial

---
+++ BarCodeReader/ZXingScreen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BarCodeReader
+{
+    public partial class ZXingScreen : ContentPage
+    {
+        public ZXingScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
