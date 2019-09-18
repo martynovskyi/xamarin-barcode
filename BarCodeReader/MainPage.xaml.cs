9d18b70c89a15a7976a65f6c43547b72ba6eb681
Vitalii Martynovskyi <vitaliy.martynovskyy@ukr.net>
2019-09-18 07:52:39 -0500

    add screen navigation

--- BarCodeReader/MainPage.xaml.cs
+++ BarCodeReader/MainPage.xaml.cs
@@ -10,91 +10,16 @@ using ZXing;
 
 namespace BarCodeReader
 {
-    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
 
-            // bar code standarts
-            //BarcodeScanner.Options = new ZXing.Mobile.MobileBarcodeScanningOptions
-            //{
-            //    PossibleFormats = new List<BarcodeFormat>() {
-            //        //BarcodeFormat.RSS_EXPANDED,
-            //        //BarcodeFormat.RSS_14,
-            //        //BarcodeFormat.EAN_13,
-            //        //BarcodeFormat.EAN_8,
-            //        BarcodeFormat.All_1D
-
-            //        //BarcodeFormat.UPC_A,
-            //        //BarcodeFormat.CODE_128,
-            //        //BarcodeFormat.DATA_MATRIX
-            //    },
-            //    TryHarder = true,
-            //    AssumeGS1 = true,
-            //    AutoRotate = true,
-
-            //};
         }
-
-        public void OnCameraClickedAsync(object sender, EventArgs args)
+        async void OpenZxing(object sender, EventArgs args)
         {
-            resultLabel.Text = "Scanning . . .";
-            barcodeType.Text = "";
-            CameraSwitcher(true);
-
-
-
-        }
-
-        public void Torch(object sender, EventArgs args)
-        {
-            BarcodeScanner.IsTorchOn = !BarcodeScanner.IsTorchOn;
-        }
-
-        private void CameraSwitcher(bool state)
-        {
-            BarcodeScanner.IsVisible = state;
-            BarcodeScanner.IsScanning = state;
-            BarcodeScanner.IsAnalyzing = state;
-
-        }
-
-        public void Handle_OnScanResult(Result result)
-        {
-            if (string.IsNullOrWhiteSpace(result.Text))
-            {
-                Vibrate(300);
-                resultLabel.Text = "Scan fail";
-                return;
-            }
-            Vibrate(100);
-            CameraSwitcher(false);
-
-            Device.BeginInvokeOnMainThread(() =>
-                {
-                    resultLabel.Text = result.Text;
-                    barcodeType.Text = result.BarcodeFormat.ToString();
-                });
-        }
-
-        private void Vibrate(int millis)
-        {
-            try
-            {
-                // Or use specified time
-                var duration = TimeSpan.FromMilliseconds(millis);
-                Vibration.Vibrate(duration);
-            }
-            catch (FeatureNotSupportedException ex)
-            {
-                // Feature not supported on device
-            }
-            catch (Exception)
-            {
-                // Other error has occurred.
-            }
+           await Navigation.PushAsync(new ZXingScreen());
         }
     }
 }
--- BarCodeReader/ZXingScreen.xaml.cs
+++ BarCodeReader/ZXingScreen.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using ZXing;
 
 namespace BarCodeReader
 {
@@ -10,6 +15,85 @@ namespace BarCodeReader
         public ZXingScreen()
         {
             InitializeComponent();
+
+            //bar code standarts
+            BarcodeScanner.Options = new ZXing.Mobile.MobileBarcodeScanningOptions
+            {
+                PossibleFormats = new List<BarcodeFormat>() {
+                    //BarcodeFormat.RSS_EXPANDED,
+                    //BarcodeFormat.RSS_14,
+                    //BarcodeFormat.EAN_13,
+                    //BarcodeFormat.EAN_8,
+                    BarcodeFormat.All_1D
+
+                    //BarcodeFormat.UPC_A,
+                    //BarcodeFormat.CODE_128,
+                    //BarcodeFormat.DATA_MATRIX
+                },
+                TryHarder = true,
+                AssumeGS1 = true,
+                AutoRotate = true,
+
+            };
+        }
+
+        public void OnCameraClickedAsync(object sender, EventArgs args)
+        {
+            resultLabel.Text = "Scanning . . .";
+            barcodeType.Text = "";
+            CameraSwitcher(true);
+
+
+
+        }
+
+        public void Torch(object sender, EventArgs args)
+        {
+            BarcodeScanner.IsTorchOn = !BarcodeScanner.IsTorchOn;
+        }
+
+        private void CameraSwitcher(bool state)
+        {
+            BarcodeScanner.IsVisible = state;
+            BarcodeScanner.IsScanning = state;
+            BarcodeScanner.IsAnalyzing = state;
+
+        }
+
+        public void Handle_OnScanResult(Result result)
+        {
+            if (string.IsNullOrWhiteSpace(result.Text))
+            {
+                Vibrate(300);
+                resultLabel.Text = "Scan fail";
+                return;
+            }
+            Vibrate(100);
+            CameraSwitcher(false);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                resultLabel.Text = result.Text;
+                barcodeType.Text = result.BarcodeFormat.ToString();
+            });
+        }
+
+        private void Vibrate(int millis)
+        {
+            try
+            {
+                // Or use specified time
+                var duration = TimeSpan.FromMilliseconds(millis);
+                Vibration.Vibrate(duration);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+            }
         }
     }
 }
